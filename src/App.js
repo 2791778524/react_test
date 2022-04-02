1245c987cce4bb70e3afbc582acc7e8b4819613c
lizilong <2791778524@qq.com>
2022-04-02 10:42:22 +0800

    clock

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import HelloWorld from './page/HelloWord/Helloworld'
+// import HelloWorld from './page/HelloWord/Helloworld'
+import Clock from './page/Clock/index'
 function App() {
   return (
     <div className="App">
-      <HelloWorld name="Mark"/>
+      {/* <HelloWorld name="Mark"/> */}
+      <Clock />
     </div>
   );
 }
---
+++ src/page/Clock/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+// import  ReactDOM  from 'react-dom'
+class Clock extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            data :new Date(),
+            colseTime:null
+        }
+    }
+    componentDidMount() {
+        let timer = null
+        let that = this
+        timer = setInterval(()=>{
+            this.setState({data: new Date()})
+        },1000)
+        this.setState(
+            {closeTime:function closeTime() {
+                if(timer == null){
+                    console.log(timer);
+                    timer = setInterval(()=>{
+                        that.setState({data: new Date()})
+                    },1000)
+                    return
+                } 
+                clearInterval(timer)
+                timer = null
+            }
+            }
+        )
+    }
+    render() {
+        return (
+            <div id='root'>
+                <div>Hello World</div>
+                <h2>It is {this.state.data.toLocaleTimeString()}.</h2>
+                <button onClick={this.state.closeTime}>stop</button>
+                <button onClick={this.state.closeTime}>start</button>
+            </div>
+        )
+    }
+}
+export default Clock

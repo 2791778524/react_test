de40a005fe5e95685185b9f3870fda88596a78eb
lizilong <2791778524@qq.com>
2022-04-06 14:40:10 +0800

    提交代码

--- src/page/Home/header/index.js
+++ src/page/Home/header/index.js
@@ -4,21 +4,55 @@ class Header extends React.Component{
     constructor(props){
         super(props)
         this.state={
-
+            data:[
+                {
+                    id:'01',
+                    value:'首页'
+                },
+                {
+                    id:'02',
+                    value:'产品分类'
+                },
+                {
+                    id:'03',
+                    value:'产品列表'
+                },
+                {
+                    id:'04',
+                    value:'产品信息'
+                },
+                {
+                    id:'05',
+                    value:'产品参数'
+                },
+                {
+                    id:'06',
+                    value:'公司简介'
+                },
+                {
+                    id:'07',
+                    value:'产品中心'
+                }
+            ]
         }
     }
     render() {
         return(
-            <div className="banner">
+            <div className="container">
+                <div className="banner">
                 <ul>
-                    <li>目录一</li>
-                    <li>目录二</li>
-                    <li>目录三</li>
-                    <li>目录四</li>
-                    <li>目录五</li>
-                    <li>目录六</li>
-                    <li>目录七</li>
+                    {this.state.data.map(item => {
+                        return(
+                            <li>{item.value}</li>
+                        )
+                    })}
                 </ul>
+                <div className="content">
+                    <div className="left">left</div>
+                    <div className="center">center</div>
+                    <div className="right">right</div>
+                </div>
+            </div>
             </div>
         )
     }
